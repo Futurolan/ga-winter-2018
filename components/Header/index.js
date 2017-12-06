c153595692a155e52aced0a73fcc0ee3ea3f6e5a
bmagic <baptiste.routhieau@gmail.com>
2017-12-06 22:36:37 +0100

    Add Lato police Header menu is responsive now.

--- components/Header/index.js
+++ components/Header/index.js
@@ -6,66 +6,87 @@ import PropTypes from 'prop-types'
 
 import stylesheet from './styles.scss'
 
-const Header = (props) => (
+class Header extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {isOpen: false}
 
-  <header className='ga-header'>
-    <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
-    <nav className='navbar is-transparent'>
+    this.toggleMenu = this.toggleMenu.bind(this)
+  }
 
-      <div className='navbar-brand'>
-        <Link href='/'>
-          <a className='navbar-item is-size-4 has-text-white'>
-            <strong>Gamers Assembly</strong>
-          </a>
-        </Link>
-        <button className='button navbar-burger is-dark'>
-          <span />
-          <span />
-          <span />
-        </button>
-      </div>
-      <div className='navbar-menu'>
-        <div className='navbar-start' />
-        <Link className='navbar-item' href='/news' >
-          <a className={classNames('navbar-item', 'has-text-white', {'is-active': props.router.pathname === '/news'})}>Actualités</a>
-        </Link>
-        <Link className='navbar-item' href='/billeterie'>
-          <a className={classNames('navbar-item', 'has-text-white', {'is-active': props.router.pathname === '/billeterie'})}>Billeterie</a>
-        </Link>
-        <Link className='navbar-item' href='/tournois'>
-          <a className={classNames('navbar-item', 'has-text-white', {'is-active': props.router.pathname === '/tournois'})}>Tournois</a>
-        </Link>
-        <Link className='navbar-item' href='/info'>
-          <a className={classNames('navbar-item', 'has-text-white', {'is-active': props.router.pathname === '/info'})}>Infos pratiques</a>
-        </Link>
-        <Link className='navbar-item' href='/partenaires'>
-          <a className={classNames('navbar-item', 'has-text-white', {'is-active': props.router.pathname === '/partenaires'})}>Partenaires</a>
-        </Link>
-        <div className='navbar-end'>
-          <div className='navbar-item'>
-            <div className='field is-grouped'>
-              <a className='control has-text-white' href='https://www.facebook.com/GamersAssembly'>
-                <i className='fa fa-facebook-official' />
+  toggleMenu () {
+    this.setState({isOpen: !this.state.isOpen})
+  }
+  render () {
+    const pathname = this.props.router.pathname
+    return (
+
+      <header className='ga-header'>
+        <style dangerouslySetInnerHTML={{__html: stylesheet}} />
+        <nav className='navbar is-transparent'>
+
+          <div className='navbar-brand'>
+            <Link href='/'>
+              <a className='navbar-item is-size-4'>
+                <strong>Gamers Assembly</strong>
               </a>
-              <a className='control has-text-white' href='https://twitter.com/GamersAssembly'>
-                <i className='fa fa-twitter' />
-              </a>
-              <a className='control has-text-white' href='https://www.flickr.com/photos/futurolan'>
-                <i className='fa fa-flickr' />
-              </a>
-              <a className='control has-text-white' href='https://twitch.tv/gamers_assembly'>
-                <i className='fa fa-twitch' />
-              </a>
-              <a className='control has-text-white' href='https://www.youtube.com/channel/UCbfhRIAsc4xdRACnDUwRfRw'>
-                <i className='fa fa-youtube-play' />
-              </a>
+            </Link>
+            <button className='button navbar-burger is-dark' onClick={this.toggleMenu}>
+              <span />
+              <span />
+              <span />
+            </button>
+          </div>
+          <div className={classNames('navbar-menu', {'is-active': this.state.isOpen})}>
+            <div className='navbar-start' />
+            <Link href='/news'>
+              <a
+                className={classNames('navbar-item', {'is-active': pathname === '/news'})}>Actualités</a>
+            </Link>
+            <Link href='/billeterie'>
+              <a
+                className={classNames('navbar-item', {'is-active': pathname === '/billeterie'})}>Billeterie</a>
+            </Link>
+            <Link href='/tournois'>
+              <a
+                className={classNames('navbar-item', {'is-active': pathname === '/tournois'})}>Tournois</a>
+            </Link>
+            <Link href='/info'>
+              <a
+                className={classNames('navbar-item', {'is-active': pathname === '/info'})}>Infos
+                  pratiques</a>
+            </Link>
+            <Link href='/partenaires'>
+              <a
+                className={classNames('navbar-item', {'is-active': pathname === '/partenaires'})}>Partenaires</a>
+            </Link>
+            <div className='navbar-end'>
+              <div className='navbar-item'>
+                <div className='field is-grouped'>
+                  <a className='control' href='https://www.facebook.com/GamersAssembly'>
+                    <i className='fa fa-facebook-official' />
+                  </a>
+                  <a className='control' href='https://twitter.com/GamersAssembly'>
+                    <i className='fa fa-twitter' />
+                  </a>
+                  <a className='control' href='https://www.flickr.com/photos/futurolan'>
+                    <i className='fa fa-flickr' />
+                  </a>
+                  <a className='control' href='https://twitch.tv/gamers_assembly'>
+                    <i className='fa fa-twitch' />
+                  </a>
+                  <a className='control' href='https://www.youtube.com/channel/UCbfhRIAsc4xdRACnDUwRfRw'>
+                    <i className='fa fa-youtube-play' />
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-    </nav>
-  </header>
-)
+        </nav>
+      </header>
+    )
+  }
+}
 
 Header.propTypes = {
   router: PropTypes.object
--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,7 @@ export default class MyDocument extends Document {
         <Head>
           <meta charSet='utf-8' />
           <meta name='viewport' content='width=device-width, initial-scale=1' />
+          <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' />
           <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
         </Head>
         <body>
