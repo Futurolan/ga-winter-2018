317e7ff185ebe4a2951cf88e25fe89c7fef59f3a
bmagic <baptiste.routhieau@gmail.com>
2018-03-18 00:06:36 +0100

    Ajout de la page recrutement.

---
+++ pages/recrutement.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Layout from 'components/Layout'
+import withData from 'lib/withData'
+import Meta from 'components/Meta'
+
+class ContactsPage extends React.Component {
+  render () {
+    return (
+      <Layout name='recrutement-page'>
+        <Meta title='Recrutement' />
+        <div className='section'>
+          <div className='container'>
+            <div className='content'>
+              <iframe frameBorder='0' height='1750' src='https://goo.gl/forms/84pHQfSp50RcuUel2' width='100%'>Chargement en cours...</iframe>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export default withData(ContactsPage)
