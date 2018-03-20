02c0eec2c386d594a879270bba0ddd665fc56908
bmagic <baptiste.routhieau@gmail.com>
2018-03-20 22:25:08 +0100

    Ajout de la description de la page liste

--- pages/news.js
+++ pages/news.js
@@ -8,7 +8,7 @@ class NewsPage extends React.Component {
   render () {
     return (
       <Layout name='news-page'>
-        <Meta title='Actualités' />
+        <Meta title='Actualités' description="Toute l'actualité de la lan" />
         <div className='section'>
           <div className='container'>
             <h1 className='title'>Actualités</h1>
