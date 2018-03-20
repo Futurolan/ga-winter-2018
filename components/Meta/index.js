caafa615bc00ce6111ea1aa1db7e7f524d9f0528
bmagic <baptiste.routhieau@gmail.com>
2018-03-20 22:16:37 +0100

    Ajout de l'image dans les métadonnées Ajout de la description et de l'image dans les métadonnées de la page news-single

--- components/Meta/index.js
+++ components/Meta/index.js
@@ -1,25 +1,27 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import Head from 'next/head'
+import { withRouter } from 'next/router'
+import PropTypes from 'prop-types'
 
 const Meta = (props) => {
   const title = props.title ? `${props.title} | Galloween 2018` : 'Galloween 2018'
   const description = props.description ? props.description : 'Retrouvez la Gamers Assembly Halloween Edition 2018"'
+  const image = props.image ? props.image : `${process.env.BASE_URL}/static/img/logo.png`
   return (
     <Head>
       <title>{title}</title>
       <meta name='description' content={description} />
-      <meta name='image' content={`${process.env.BASE_URL}/static/img/logo.png`} />
+      <meta name='image' content={image} />
       <meta itemProp='name' content={title} />
       <meta itemProp='description' content={description} />
-      <meta itemProp='image' content={`${process.env.BASE_URL}/static/img/logo.png`} />
+      <meta itemProp='image' content={image} />
       <meta name='twitter:card' content='summary' />
       <meta name='twitter:title' content={title} />
       <meta name='twitter:description' content={description} />
       <meta name='og:title' content={title} />
       <meta name='og:description' content={description} />
-      <meta name='og:image' content={`${process.env.BASE_URL}/static/img/logo.png`} />
-      {/* <meta name='og:url' content={window.location.href} /> */}
+      <meta name='og:image' content={image} />
+      <meta name='og:url' content={`${process.env.BASE_URL}${props.router.asPath}`} />
       <meta name='og:site_name' content={title} />
       <meta name='og:locale' content='fr_FR' />
       <meta name='og:type' content='website' />
@@ -29,10 +31,9 @@ const Meta = (props) => {
 
 Meta.propTypes = {
   title: PropTypes.string,
-  description: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.bool
-  ])
+  description: PropTypes.string,
+  image: PropTypes.string,
+  router: PropTypes.object
 }
 
-export default Meta
+export default withRouter(Meta)
--- components/NewsContent/index.js
+++ components/NewsContent/index.js
@@ -6,23 +6,23 @@ import stylesheet from './styles.scss'
 import Moment from 'react-moment'
 import Meta from 'components/Meta'
 
-function NewsContent ({data: { loading, error, nodeById }}) {
+function NewsContent ({data: { loading, error, node }}) {
   if (error) return <div>Error Loading News</div>
 
-  if (nodeById) {
+  if (node) {
     return <div className='ga-news-content'>
       <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
-      <Meta title={nodeById.title} />
+      <Meta title={node.title} image={node.image.fullhd.url} description={node.description}/>
       <section className='section'>
 
         <div className='container'>
           <figure className='image is-5by1'>
-            <img srcSet={`${nodeById.image.mobile.url} 705w, ${nodeById.image.desktop.url} 960w, ${nodeById.image.widescreen.url} 1155w, ${nodeById.image.fullhd.url} 1345w`} />
+            <img srcSet={`${node.image.mobile.url} 705w, ${node.image.desktop.url} 960w, ${node.image.widescreen.url} 1155w, ${node.image.fullhd.url} 1345w`} />
           </figure>
-          <div className='is-pulled-right'> Créé le <Moment unix format='DD/MM/YYYY à HH:SS'>{nodeById.created}</Moment>, par {nodeById.entityOwner.name}</div>
+          <div className='is-pulled-right'> Créé le <Moment unix format='DD/MM/YYYY à HH:SS'>{node.created}</Moment>, par {node.entityOwner.name}</div>
           <div className='content has-text-justified' >
-            <h1 className='title '> {nodeById.title}</h1>
-            <div dangerouslySetInnerHTML={{__html: nodeById.content.processed}} />
+            <h1 className='title '> {node.title}</h1>
+            <div dangerouslySetInnerHTML={{__html: node.content.processed}} />
           </div>
         </div>
 
@@ -35,7 +35,7 @@ function NewsContent ({data: { loading, error, nodeById }}) {
 export const news = gql`
 
   query news($nid:String!) {
-    nodeById(id: $nid) {
+    node:nodeById(id: $nid) {
       ... on NodeNews {
         title
         entityOwner {
@@ -45,6 +45,7 @@ export const news = gql`
         content:fieldNewsContent{
           processed
         }
+        description:fieldNewsDescription
         image:fieldNewsImage{
           mobile:derivative(style:crop_5_1_705x141){
             url
