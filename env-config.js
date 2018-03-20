6e2a23c972e5191b6aaa7b11366a93a949f50f84
bmagic <baptiste.routhieau@gmail.com>
2018-03-20 22:20:34 +0100

    Fix l'import des conf par défaut

--- env-config.js
+++ env-config.js
@@ -1,7 +1,6 @@
-const prod = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  'process.env.BACKEND_API_URL': prod ? process.env.BACKEND_API_URL : 'http://localhost:8080',
-  'process.env.EDITION_ID': prod ? process.env.EDITION_ID : 1,
-  'process.env.BASE_URL': prod ? process.env.BASE_URL : 'http://localhost:3000'
+  'process.env.BACKEND_API_URL': process.env.BACKEND_API_UR ? process.env.BACKEND_API_URL : 'http://localhost:8080',
+  'process.env.EDITION_ID': process.env.EDITION_ID ? process.env.EDITION_ID : 1,
+  'process.env.BASE_URL': process.env.BASE_URL ? process.env.BASE_URL : 'http://localhost:3000'
 }
