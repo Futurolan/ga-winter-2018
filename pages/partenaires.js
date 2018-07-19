b3e33245c5bf7e68cdcba8873ec45e50f7dcd61e
bmagic <baptiste.routhieau@gmail.com>
2018-07-19 21:20:44 +0200

    Add metadata to partners page

--- pages/partenaires.js
+++ pages/partenaires.js
@@ -3,13 +3,14 @@ import Layout from 'components/Layout'
 import Meta from 'components/Meta'
 import PartnerList from 'components/PartnerList'
 import '../styles/styles.scss'
+import config from '../lib/config'
 
 class PartenairesPage extends React.Component {
   render () {
     return (
       <Layout name='partenaires-page has-bg-star'>
         <div className='section'>
-          <Meta title='Partenaires' />
+          <Meta title={config.partners.title} description={config.partners.description} />
           <div className='container has-text-centered'>
             <h1 className='title title-line'><span>Partenaires</span></h1>
             <PartnerList />
