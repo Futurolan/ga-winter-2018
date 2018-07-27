01985a1adbdff396a22e2b7b8ae405ac93febd92
bmagic <baptiste.routhieau@gmail.com>
2018-07-27 11:27:49 +0200

    Add meta to informations page

--- pages/infos.js
+++ pages/infos.js
@@ -3,13 +3,14 @@ import Layout from 'components/Layout'
 import Meta from 'components/Meta'
 import InfosContent from 'components/InfosContent'
 import '../styles/styles.scss'
+import config from '../lib/config'
 
 class InfosPage extends React.Component {
   render () {
     return (
       <Layout name='infos-page has-bg-star'>
         <div className='section'>
-          <Meta title='Informations pratiques' />
+          <Meta title={config.infos.title} description={config.infos.description} />
           <div className='container'>
             <h1 className='title title-line has-text-centered'><span>Informations pratiques</span></h1>
             <InfosContent />
