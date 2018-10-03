0b7f16eb77d8fbe92734934c16b98ab24471c10f
mupfpilou <41705573+mupfpilou@users.noreply.github.com>
2018-10-03 21:03:19 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -35,7 +35,7 @@ let config = {
     description: 'Toutes les informations sur cet évènement'
   },
   tickets: {
-    title: 'Billeterie',
+    title: 'Billetterie',
     description: 'Achetez vos places pour la Gamers Assembly Winter Edition 2018'
   },
   contact: {
