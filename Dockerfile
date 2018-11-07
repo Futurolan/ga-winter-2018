5e3d1cfa11ba275374f983e4055c9a3200a9155b
bmagic <baptiste@bmagic.fr>
2018-11-07 09:55:23 +0100

    Update UA Code Fix Dockerfile with correct URL

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ EXPOSE 3000
 ARG EDITION_ID=40
 ARG BACKEND_API_URL=https://backoffice.gamers-assembly.net
 ARG BACKEND_LOCAL_API_URL=http://drupal
-ARG BASE_URL=https://testgawe.gamers-assembly.net
+ARG BASE_URL=https://winter2018.gamers-assembly.net
 
 WORKDIR /usr/src/ga-client
 COPY . .
--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ let config = {
   logo: `/static/img/logo-100x100.png`,
   description: 'L\'édition 2018 de la Gamers Assembly Winter Edition aura lieu du 1er au 2 décembre. À la Salle de l\'Agora - Jaunay-Marigny (86).',
   metaImage: `${process.env.BASE_URL}/static/img/logo-200x200.png`,
-  gaTrackingId: 'UA-7594737-17',
+  gaTrackingId: 'UA-128777022-4',
   mainPartner: {
     logo: '/static/img/logoJM106.png',
     url: 'http://www.jaunay-marigny.fr/'
